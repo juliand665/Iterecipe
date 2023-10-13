dd2eb39ba604f626284132c44f092c25a0a199c9
Julian Dunskus <julian.dunskus@gmail.com>
2023-10-13 14:47:11 +0200

    fix undo/redo buttons not updating

--- Iterecipe/UndoRedoButtons.swift
+++ Iterecipe/UndoRedoButtons.swift
@@ -2,22 +2,30 @@ import SwiftUI
 
 struct UndoRedoButtons: View {
 	@Environment(\.undoManager) private var undoManager
+	@State private var updateTrigger = false
 	
 	var body: some View {
 #if !os(macOS)
-		Button {
-			undoManager!.undo()
-		} label: {
-			Label("Undo", systemImage: "arrow.uturn.backward.circle")
+		if let undoManager {
+			let _ = updateTrigger // trigger view updates when this is toggled
+			
+			Button {
+				undoManager.undo()
+			} label: {
+				Label("Undo", systemImage: "arrow.uturn.backward.circle")
+			}
+			.disabled(!undoManager.canUndo)
+			.onReceive(NotificationCenter.default.publisher(for: .NSUndoManagerWillCloseUndoGroup, object: undoManager)) { _ in updateTrigger.toggle() }
+			.onReceive(NotificationCenter.default.publisher(for: .NSUndoManagerWillUndoChange, object: undoManager)) { _ in updateTrigger.toggle() }
+			.onReceive(NotificationCenter.default.publisher(for: .NSUndoManagerWillRedoChange, object: undoManager)) { _ in updateTrigger.toggle() }
+			
+			Button {
+				undoManager.redo()
+			} label: {
+				Label("Redo", systemImage: "arrow.uturn.forward.circle")
+			}
+			.disabled(!undoManager.canRedo)
 		}
-		.disabled(undoManager?.canUndo != true)
-		
-		Button {
-			undoManager!.redo()
-		} label: {
-			Label("Redo", systemImage: "arrow.uturn.forward.circle")
-		}
-		.disabled(undoManager?.canRedo != true)
 #endif
 	}
 }
