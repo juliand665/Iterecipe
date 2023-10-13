fe0c1782d2ee087fb76185804f06823edde085a4
Julian Dunskus <julian.dunskus@gmail.com>
2023-10-13 16:40:23 +0200

    improve preview data

--- Iterecipe/Recipe.swift
+++ Iterecipe/Recipe.swift
@@ -1,4 +1,5 @@
 import Foundation
+import ArrayBuilder
 import HandyOperators
 
 struct Recipe: Codable {
@@ -12,7 +13,7 @@ struct Recipe: Codable {
 		revisions.append(revisions.last! <- { $0.notes = [] })
 	}
 	
-	struct Revision: Codable {
+	struct Revision: Codable, Identifiable {
 		var id = ObjectID<Self>()
 		var dateCreated = Date()
 		var ingredients: [TextItem] = []
@@ -65,18 +66,18 @@ struct ObjectID<Object>: Hashable, Codable {
 extension Recipe {
 	static let example = Self(
 		title: "Example Recipe",
-		source: "www.example.com/recipes/example",
+		source: "example.com/recipes/example",
 		image: nil,
-		revisions: [
-			.init(
+		revisions: .init {
+			let initial = Revision(
+				dateCreated: .init(timeIntervalSinceNow: -10_000),
 				ingredients: [
 					"42 g sugar",
 					"0.5 tsp salt",
 					"freshly-ground black pepper",
 					"freshly-ground black pepper again but this time it's much longer",
-					"250 mL milk",
-					"3 eggs",
-					"1 stick butter, melted, browned to a dark amber color",
+					"2 eggs",
+					"1 stick butter, melted",
 				],
 				steps: [
 					"The first step.",
@@ -85,8 +86,26 @@ extension Recipe {
 					"The fourth step.",
 					"One final step to finish it all up and get it out there after all this work to wrap to a new line.",
 				],
-				notes: ["a note", "another note that is considerably longer and thus takes multiple lines to display"]
+				notes: ["an old note"]
 			)
-		]
+			
+			initial
+			initial <- {
+				$0.dateCreated = .now
+				
+				$0.ingredients.remove(at: 3)
+				$0.ingredients.insert("250 mL milk", at: 4)
+				$0.ingredients[3].text = "3 eggs"
+				$0.ingredients[5].text += "1 stick butter, melted, browned to a dark amber color"
+				
+				$0.steps.insert("Actually i forgot something important!", at: 2)
+				$0.steps[5].text += " This is the best part."
+				
+				$0.notes = [
+					"a note",
+					"another note that is considerably longer and thus takes multiple lines to display"
+				]
+			}
+		}
 	)
 }
